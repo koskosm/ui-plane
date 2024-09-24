ecd3f6a7c12c3938f8da31abb62908dd613151f3
Frank Lam <franklam@oursky.com>
2024-09-25 05:19:33 +0800

    Improve searchbox style

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -25,14 +25,14 @@ function SearchBox({ onSearch, searchTerm, onClear }) {
         <div className="flex">
           <input
             type="text"
-            placeholder="Search anything about UI design"
+            placeholder="Search here..."
             value={localSearchTerm}
             onChange={handleChange}
-            className="w-full px-4 py-2 text-white bg-[#1D2226] border rounded-l-lg focus:outline-none focus:border-blue-500"
+            className="w-full px-4 py-2 text-white bg-[#1D2226] border rounded-l-full focus:outline-none focus:border-blue-500"
           />
           <button
             type="submit"
-            className="px-4 py-2 text-black bg-white rounded-r-lg hover:bg-white focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+            className="px-4 py-2 text-black bg-white rounded-r-full hover:bg-white focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
           >
             Search
           </button>
