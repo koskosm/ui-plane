d6bb1804a2efd1c6be7ba27a68bd3c79b26ac361
Frank Lam <franklam@oursky.com>
2024-09-25 03:31:45 +0800

    Keep title when search

--- src/App.js
+++ src/App.js
@@ -19,11 +19,12 @@ function ScrollToTop() {
   return null;
 }
 
-function App() {
+function AppContent() {
   const [searchResults, setSearchResults] = useState(null);
   const [searchTerm, setSearchTerm] = useState('');
   const [isFilterOpen, setIsFilterOpen] = useState(false);
   const categories = [...new Set(posts.flatMap(post => post.tags))];
+  const location = useLocation();
 
   const handleSearch = (term) => {
     setSearchTerm(term);
@@ -44,83 +45,84 @@ function App() {
     setIsFilterOpen(!isFilterOpen);
   };
 
+  const showHeaderContent = !location.pathname.startsWith('/post/');
+
   return (
-    <Router>
-      <ScrollToTop />
-      <div className="App flex flex-col min-h-screen bg-[#1D2226] text-white font-basier">
-        <Navbar />
-        <div className="flex-grow flex">
-          <div className="flex-grow flex flex-col overflow-x-hidden">
-            <main className="flex-grow px-4 lg:px-8 pb-12">
-              <div className="max-w-7xl mx-auto mt-8">
-                <Routes>
-                  <Route path="/" element={
-                    <>
-                      <div className="text-left mb-12">
-                        <p className='pb-12'><span className="custom-emoji text-8xl" role="img" aria-label="UI Plane">✈️</span></p>
-                        <p className="text-6xl md:text-6xl mb-8 pb-20">
-                          A UI flight checklist and collection of best design practices!
-                        </p>
-                        <div className="flex flex-col items-start">
-                          <div className="flex items-center w-full">
-                            <SearchBox onSearch={handleSearch} searchTerm={searchTerm} onClear={handleClearSearch} />
-                            <div className="relative ml-4 grow justify-end flex">
-                              <button
-                                onClick={toggleFilter}
-                                className="px-4 py-2 border border-white text-white rounded-full hover:bg-white hover:text-black transition-colors duration-200"
-                              >
-                                Filter
-                              </button>
-                              {isFilterOpen && (
-                                <div className="absolute right-0 mt-12 w-64 bg-white text-black rounded-md shadow-lg z-10 max-h-60 overflow-scroll">
-                                  <div className="py-2">
-                                    {categories.map((category, index) => (
-                                      <Link
-                                        key={index}
-                                        to={`/category/${encodeURIComponent(category)}`}
-                                        className="block px-4 py-2 text-sm hover:bg-gray-100"
-                                        onClick={() => setIsFilterOpen(false)}
-                                      >
-                                        {category}
-                                      </Link>
-                                    ))}
-                                  </div>
-                                </div>
-                              )}
+    <div className="App flex flex-col min-h-screen bg-[#1D2226] text-white font-basier">
+      <Navbar />
+      <div className="flex-grow flex">
+        <div className="flex-grow flex flex-col overflow-x-hidden">
+          <main className="flex-grow px-4 lg:px-8 pb-12">
+            <div className="max-w-7xl mx-auto mt-8">
+              {showHeaderContent && (
+                <div className="text-left mb-12">
+                  <p className='pb-12'><span className="custom-emoji text-8xl" role="img" aria-label="UI Plane">✈️</span></p>
+                  <p className="text-6xl md:text-6xl mb-8 pb-20">
+                    A UI flight checklist and collection of best design practices!
+                  </p>
+                  <div className="flex flex-col items-start">
+                    <div className="flex items-center w-full">
+                      <SearchBox onSearch={handleSearch} searchTerm={searchTerm} onClear={handleClearSearch} />
+                      <div className="relative ml-4 grow justify-end flex">
+                        <button
+                          onClick={toggleFilter}
+                          className="px-4 py-2 border border-white text-white rounded-full hover:bg-white hover:text-black transition-colors duration-200"
+                        >
+                          Filter
+                        </button>
+                        {isFilterOpen && (
+                          <div className="absolute right-0 mt-12 w-64 bg-white text-black rounded-md shadow-lg z-10 max-h-60 overflow-scroll">
+                            <div className="py-2">
+                              {categories.map((category, index) => (
+                                <Link
+                                  key={index}
+                                  to={`/category/${encodeURIComponent(category)}`}
+                                  className="block px-4 py-2 text-sm hover:bg-gray-100"
+                                  onClick={() => setIsFilterOpen(false)}
+                                >
+                                  {category}
+                                </Link>
+                              ))}
                             </div>
                           </div>
-                        </div>
+                        )}
                       </div>
-                      {searchResults ? (
-                        <>
-                          <div className="flex justify-between items-center mb-4">
-                            <h2 className="text-2xl font-bold">Search Results for "{searchTerm}"</h2>
-                            <button 
-                              onClick={handleClearSearch}
-                              className="px-3 py-1 text-sm text-white bg-gray-700 rounded hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50"
-                            >
-                              Clear Search
-                            </button>
-                          </div>
-                          <PostList posts={searchResults} />
-                        </>
-                      ) : (
-                        <PostList posts={posts} />
-                      )}
-                    </>
-                  } />
-                  <Route path="/category/:tag" element={<CategoryPage posts={posts} />} />
-                  <Route path="/post/:slug" element={<PostWrapper posts={posts} />} />
-                </Routes>
-              </div>
-            </main>
-            <div className="w-full">
-              <Footer />
+                    </div>
+                  </div>
+                </div>
+              )}
+              <Routes>
+                <Route path="/" element={
+                  <>
+                    {searchResults ? (
+                      <>
+                        <div className="flex justify-between items-center mb-4">
+                          <h2 className="text-2xl font-bold">Search Results for "{searchTerm}"</h2>
+                          <button 
+                            onClick={handleClearSearch}
+                            className="px-3 py-1 text-sm text-white bg-gray-700 rounded hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50"
+                          >
+                            Clear Search
+                          </button>
+                        </div>
+                        <PostList posts={searchResults} />
+                      </>
+                    ) : (
+                      <PostList posts={posts} />
+                    )}
+                  </>
+                } />
+                <Route path="/category/:tag" element={<CategoryPage posts={posts} />} />
+                <Route path="/post/:slug" element={<PostWrapper posts={posts} />} />
+              </Routes>
             </div>
+          </main>
+          <div className="w-full">
+            <Footer />
           </div>
         </div>
       </div>
-    </Router>
+    </div>
   );
 }
 
@@ -133,6 +135,15 @@ function PostWrapper({ posts }) {
   }
 
   return <PostContent post={post} />;
+}
+
+function App() {
+  return (
+    <Router>
+      <ScrollToTop />
+      <AppContent />
+    </Router>
+  );
 }
 
 export default App;
